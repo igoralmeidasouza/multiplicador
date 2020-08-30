fa0ac39322c76ca5fca261958defbae569c96a7c
igoralmeidasouza <67607583+igoralmeidasouza@users.noreply.github.com>
2020-08-30 17:47:12 -0300

    Initial commit

---
+++ src/mult.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class mult {
+     public static void main(String args[]){
+        Test();
+    }
+    public static void Test(){
+        double n1, n2, total=0;
+        String SysInp="";
+
+        SysInp = JOptionPane.showInputDialog(null, "primeiro numero: ");
+        n1= Double.parseDouble(SysInp);
+        SysInp = JOptionPane.showInputDialog(null, "segundo numero: ");
+        n2 = Double.parseDouble(SysInp);
+
+        total = n1 * n2;
+        JOptionPane.showMessageDialog(null, "resposta é: " + total);
+    }
+}
